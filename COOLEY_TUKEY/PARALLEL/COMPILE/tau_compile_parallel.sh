3138b6e99a5ac1c10e52b61a4919a63a82f9cc10
FlavioBobba99 <flaviobobba@gmail.com>
2024-12-03 14:04:54 +0100

    added tau

---
+++ COOLEY_TUKEY/PARALLEL/COMPILE/tau_compile_parallel.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Output name for the compiled program
+OUTPUT_NAME="../parallel_tau"
+
+# Source files to compile
+SRC_FILES="../main_parallel.c ../PARALLEL_PROGRAM_LIBS/matrix_utilities.c ../PARALLEL_PROGRAM_LIBS/FFTs.c ../PARALLEL_PROGRAM_LIBS/images_handling.c"
+
+# GCC compiler flags
+CFLAGS="-Wall"
+
+LIBFLAG="-lm"
+
+# Compile the program
+echo "Compiling the program..."
+taucc $CFLAGS $SRC_FILES -o $OUTPUT_NAME $LIBFLAG
+
+# Check if compilation was successful
+if [ $? -eq 0 ]; then
+    echo "Compilation successful."
+    echo "Output: $OUTPUT_NAME"
+else
+    echo "Compilation failed."
+fi
